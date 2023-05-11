c97588a635bc9696a114b009d330df75cfc0cfa8
NguyenALuy <s3891919@rmit.edu.vn>
2023-05-11 14:24:22 +0700

    add some files

---
+++ ecommerce-system/index.js
@@ -0,0 +1,21 @@
+// This load the products as a javascript object from products.js file
+// let products = require('./products');
+// console.log(products);
+
+// Fill in your code here to build NodeJS + Express + EJS web application
+const express = require('express');
+const app = express();//get the object of express out
+const port=3000;
+app.set('view engine', 'ejs');
+//set up view engine to ejs
+app.use(express.static('public'));
+//when working with the POST method have to include the URLENCODED
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res)=>{
+    res.render('login-form');
+})
+
+app.listen(port, () => {
+    console.log(`Listening to the port ${port}`);
+})
---
+++ ecommerce-system/public/js/script.js
@@ -0,0 +1,82 @@
+// Conditions checking the registration page
+const username = document.querySelector('#username');
+const password = document.getElementById('password');
+const realname = document.getElementById('realname');
+const address = document.getElementById('address');
+const errorName = document.querySelector('.username-err');
+const errorPw = document.querySelector('.userpassword-err');
+const errorChar = document.querySelector('.char-err');
+
+
+
+username.addEventListener('input', checkRegexName);
+password.addEventListener('input', checkRegexPassword);
+realname.addEventListener('input', checkRegexChar);
+address.addEventListener('input', checkRegexChar);
+
+function checkErrorName(valid, error){
+    if (valid) {
+        error.style.display = 'none';
+    } else {
+        error.innerHTML = "Please enter the valid name";
+        error.style.display = 'block';
+        error.style.color = "red";
+    }
+}
+function checkRegexName(event) {
+    const patternName = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,15}$/;
+    const currentValue = event.target.value;
+    const valid = patternName.test(currentValue);
+    checkErrorName(valid, errorName);
+};
+
+function checkErrorPassword(valid, error){
+    if (valid) {
+        error.style.display = 'none';
+    } else {
+        error.innerHTML = "Please enter the valid password";
+        error.style.display = 'block';
+        error.style.color = "red";
+    }
+}
+function checkRegexPassword (event) {
+    const patternName = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/;
+    const currentValue = event.target.value;
+    const valid = patternName.test(currentValue);
+    checkErrorPassword(valid, errorPw);
+};
+
+function checkErrorChar(valid, error){
+    if (valid) {
+        error.style.display = 'none';
+    } else {
+        error.innerHTML = "Please enter min 5 characters";
+        error.style.display = 'block';
+        error.style.color = "red";
+    }
+}
+
+function checkRegexChar (event) {
+    const patternName = /^.{5,}$/;
+    const currentValue = event.target.value;
+    const valid = patternName.test(currentValue);
+    checkErrorChar(valid, errorChar);
+};
+
+//Check confirmation password
+const confirm_password = document.getElementById('confirm-pass');
+confirm_password.addEventListener('input', validate_password);
+function validate_password() {
+    const pass = document.getElementById('password').value;
+    const confirm_pass = document.getElementById('confirm-pass').value;
+    if (pass != confirm_pass) {
+        document.getElementById('wrong-pass').style.color = 'red';
+        document.getElementById('wrong-pass').innerHTML
+            = '☒ Use same password';
+    } else {
+        document.getElementById('wrong-pass').style.color = 'green';
+        document.getElementById('wrong-pass').innerHTML =
+            '🗹 Password Matched';
+    }
+}
+
